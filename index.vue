3b405f053e8b9ace0c488032b746fbb54a3d9d58
se7en <373084212@qq.com>
2017-12-18 11:49:52 +0800

    init

---
+++ index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>Hello {{ World }}!<ComponentA /></div>
+</template>
+
+<script>
+import ComponentA from './ComponentA';
+
+export default {
+    data() {
+        return {
+            World: 'World'
+        }
+    },
+    components: {
+        ComponentA
+    }
+};
+</script>
+
+
+<style>
+    div {
+        color: red;
+    }
+</style>
+
+<style scoped>
+    div {
+        font-size: 32px;
+    }
+</style>
